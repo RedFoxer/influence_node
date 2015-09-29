f845db3313e526fc1272e7b811b2782cfedc8104
RAULT Alexis <web.rault@gmail.com>
2015-09-29 18:07:41 +0200

    Evaluation Node.js
    setInterval

--- app.js
+++ app.js
@@ -1,24 +1,56 @@
-var app = require('express')();
-var http = require('http').Server(app);
+var express = require('express');
+var app = express();
+var choices = require('./choices.json');
 
-// Je met socket.io dans le serveur http
+var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/',function(req,res){
-   // __dirname supergolobale
+app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(3000,function(){
-    console.log('listening on *3000');
+
+var votes = null;
+var start = function (i, socket) {
+    votes =  choices[i];
+    votes.left.total = 0;
+    votes.right.total = 0;
+    io.sockets.emit('votes', votes);
+};
+
+setInterval(function(){
+    var rand = Math.floor(Math.random() * choices.length);
+    start(rand);
+}, 5000);
+
+start(0);
+
+io.on('connection', function(socket){
+    // send all data
+    socket.emit('votes', votes);
+
+    socket.on('choice', function(what){
+        if (what === 'left') {
+            votes.left.total++;
+        } else {
+            votes.right.total++;
+        }
+        io.sockets.emit('total', votes);
+    });
+
+    socket.on('unchoice', function(what){
+        console.log('what');
+        if (what === 'left') {
+            votes.left.total--;
+        } else {
+            votes.right.total--;
+        }
+        io.sockets.emit('total', votes);
+    });
+
 });
 
-io.on('connection',function(socket){
-    var choices ='';
-    console.log('a user is connected');
-        socket.on('choice',function(what){
-            choices += ' '+what;
-            console.log('chosen: ' +what);
-            socket.emit('choices', choices);
-    });
+http.listen(3000, function(){
+    console.log('listening on *:3000');
 });
+
